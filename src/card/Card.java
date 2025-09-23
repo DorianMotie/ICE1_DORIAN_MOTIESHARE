8067089cd2ce1ea1cde3cdf46516eaff68c483c3
DorianMotie <dorianmotieshare@gmail.com>
2025-09-23 12:44:37 -0400

    added name and student number

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,6 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+dDorian Motieshare
+991783685
  */
 package card;
 
